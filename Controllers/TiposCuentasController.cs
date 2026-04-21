ec4c967846307716a535af41f5cb8ff802aa2437
Junior García <95927731+Juniorwebprogrammer@users.noreply.github.com>
2026-04-21 13:45:37 +0100

    Seccion 5 proyecto 2 CRUD con Dapper

--- Controllers/TiposCuentasController.cs
+++ Controllers/TiposCuentasController.cs
@@ -10,10 +10,25 @@ namespace ManejoPresupuesto.Controllers
     {
         private readonly string connectionString;
         private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IServiciosUsuarios serviciosUsuarios;
 
-        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas)
+        public TiposCuentasController(
+            IRepositorioTiposCuentas repositorioTiposCuentas,
+            IServiciosUsuarios serviciosUsuarios
+        )
         {
-           this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.serviciosUsuarios = serviciosUsuarios;
+        }
+        // Acciones del controlador
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+
+            return View(tiposCuentas);
         }
 
         public IActionResult Crear()
@@ -21,18 +36,131 @@ namespace ManejoPresupuesto.Controllers
             return View();
         }
 
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            return View(tipoCuenta);
+        }
+        
+        // Métodos del controlador
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(tipoCuenta);
+        }
+
         [HttpPost]
-        public IActionResult Crear(TipoCuenta tipoCuenta)
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
         {
             if (!ModelState.IsValid)
             {
                 return View(tipoCuenta);
             }
 
-            tipoCuenta.UsuarioId = 1;
-            repositorioTiposCuentas.Crear(tipoCuenta);
+            tipoCuenta.UsuarioId = serviciosUsuarios.ObtenerUsuarioId();
 
-            return View();
+            var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(
+                tipoCuenta.Nombre,
+                tipoCuenta.UsuarioId
+            );
+
+            if (yaExisteTipoCuenta)
+            {
+                ModelState.AddModelError(nameof(TipoCuenta.Nombre), "Ya existe un tipo de cuenta con ese nombre.");
+
+                return View(tipoCuenta);
+            }
+
+            await repositorioTiposCuentas.Crear(tipoCuenta);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+            
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            
+            var tipoCuentaExiste = await repositorioTiposCuentas.ObtenerPorId(
+                tipoCuenta.Id,
+                usuarioId
+            );
+
+            if (tipoCuentaExiste is null)
+            {
+                return RedirectToAction("No encontrado", "Home");
+            }
+
+            await repositorioTiposCuentas.Actualizar(tipoCuenta);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrarTipoCuenta(int id)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+            await repositorioTiposCuentas.Borrar(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VerificarExisteTipoCuenta(string nombre)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+
+            var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(nombre, usuarioId);
+
+            if (yaExisteTipoCuenta)
+            {
+                return Json($"El nombre {nombre} ya existe");
+            }
+
+            return Json(true);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            var usuarioId = serviciosUsuarios.ObtenerUsuarioId();
+
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+
+            var idsTiposCuentas = tiposCuentas.Select(x => x.Id);
+
+            var idsTiposCuentasNoPertenecenAlUsuario = ids.Except(idsTiposCuentas).ToList();
+
+            if (idsTiposCuentasNoPertenecenAlUsuario.Count > 0)
+            {
+                return Forbid();
+            }
+
+            var tiposCuentasOrdenados = ids.Select((valor, indice) => new TipoCuenta() { Id = valor, Orden = indice + 1 });
+        
+            await repositorioTiposCuentas.Ordenar(tiposCuentasOrdenados);
+
+            return Ok();
         }
     }
 }
---
+++ Services/ServiciosUsuarios.cs
@@ -0,0 +1,15 @@
+﻿namespace ManejoPresupuesto.Services
+{
+    public interface IServiciosUsuarios
+    {
+        int ObtenerUsuarioId();
+    }
+
+    public class ServiciosUsuarios: IServiciosUsuarios
+    {
+        public int ObtenerUsuarioId()
+        {
+            return 1;
+        }
+    }
+}
